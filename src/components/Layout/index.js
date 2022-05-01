82530b1ffe68d53817de189d4a3d7332c04e6e8f
gabriel antunes <gabriel.antunes@acad.pucrs.br>
2022-05-01 18:10:07 -0300

    Projeto labenu whatslab

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from './styles';
+import Main from '../Main';
+
+
+const Layout = () => {
+    return (
+        <Container>
+            <Main />
+        </Container>
+    );
+};
+export default Layout
